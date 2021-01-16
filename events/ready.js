66b7e27247dc6a133d98f34359e4efb288e03a74
aldiboytons <76821565+aldiboytons@users.noreply.github.com>
2021-01-16 14:37:02 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Aldi Boytons", {
+  await client.user.setActivity("Aldi Boytons  ラ𝙍𝙄𝙎𝙀  ₮𝙏𝘼𝙓", {
     type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
