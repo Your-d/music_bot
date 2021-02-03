3d93b30c8e8f1a64042d880deb2cdd297f550867
aldiboytons <76821565+aldiboytons@users.noreply.github.com>
2021-02-03 08:56:29 +0700

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -13,16 +13,16 @@ module.exports = {
     const channel = message.member.voice.channel
     if (!channel)return sendError("I'm sorry but you need to be in a voice channel to play music!", message.channel);
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue)return sendError("There is nothing playing that I could skip for you.", message.channel);
+    if (!serverQueue)return sendError("Gaada lagu lagi kalo di skip.", message.channel);
         if(!serverQueue.connection)return
 if(!serverQueue.connection.dispatcher)return
      if (serverQueue && !serverQueue.playing) {
       serverQueue.playing = true;
       serverQueue.connection.dispatcher.resume();
       let xd = new MessageEmbed()
-      .setDescription("▶ Resumed the music for you!")
-      .setColor("YELLOW")
-      .setTitle("Music has been Resumed!")
+      .setDescription("▶ Lanjutin lagu nye!")
+      .setColor("BLACK")
+      .setTitle("Udah nih gue lanjutin lagunye!")
        
    return message.channel.send(xd).catch(err => console.log(err));
       
@@ -36,6 +36,6 @@ if(!serverQueue.connection.dispatcher)return
         message.client.queue.delete(message.guild.id);
         return sendError(`:notes: The player has stopped and the queue has been cleared.: ${error}`, message.channel);
       }
-    message.react("✅")
+    message.react("🖕")
   },
 };
