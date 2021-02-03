0b259689db59f0227af5cbffbd7c6f76e0ee112d
aldiboytons <76821565+aldiboytons@users.noreply.github.com>
2021-02-03 09:59:09 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
   await client.user.setActivity(" di!help | Aldi Boytons ", {
-    type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
+    type: "PLAYING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
