7d55e32c05a6a2ab1ad347ac35bb543911f9dba6
aldiboytons <76821565+aldiboytons@users.noreply.github.com>
2021-02-03 09:08:23 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Warkop x ラ𝙍𝙄𝙎𝙀 x Aldi Boytons", {
+  await client.user.setActivity("di!help | Aldi Boytons ", {
     type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
