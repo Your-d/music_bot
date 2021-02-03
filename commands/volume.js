7602d71928f1f5debcb21e1d51d2ca918ad34b4d
aldiboytons <76821565+aldiboytons@users.noreply.github.com>
2021-02-03 09:02:54 +0700

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -20,7 +20,7 @@ module.exports = {
     serverQueue.volume = args[0]; 
     serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
     let xd = new MessageEmbed()
-    .setDescription(`I set the volume to: **${args[0]/1}/100**`)
+    .setDescription(`Udah gua atur jadi : **${args[0]/1}/100**`)
     .setAuthor("Server Volume Manager", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
     .setColor("BLACK")
     return message.channel.send(xd);
