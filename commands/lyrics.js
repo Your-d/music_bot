6e805b5db66d2f6328e99bde4ac393b96caa019e
aldiboytons <76821565+aldiboytons@users.noreply.github.com>
2021-02-03 10:14:41 +0700

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -12,21 +12,21 @@ module.exports = {
 
   run: async function (client, message, args) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return sendError("There is nothing playing.",message.channel).catch(console.error);
+    if (!queue) return sendError("Gaada yang di puter kntl.",message.channel).catch(console.error);
 
     let lyrics = null;
 
     try {
       lyrics = await lyricsFinder(queue.songs[0].title, "");
-      if (!lyrics) lyrics = `No lyrics found for ${queue.songs[0].title}.`;
+      if (!lyrics) lyrics = `Gaada liriknya nih lagu nye ${queue.songs[0].title}.`;
     } catch (error) {
-      lyrics = `No lyrics found for ${queue.songs[0].title}.`;
+      lyrics = `Gaada liriknya nih lagu nye ${queue.songs[0].title}.`;
     }
 
     let lyricsEmbed = new MessageEmbed()
-      .setAuthor(`${queue.songs[0].title} — Lyrics`, "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
+      .setAuthor(`${queue.songs[0].title} — Lyrics`, "https://raw.githubusercontent.com/aldiboytons/boytons/master/assets/Music.gif")
       .setThumbnail(queue.songs[0].img)
-      .setColor("YELLOW")
+      .setColor("BLACK")
       .setDescription(lyrics)
       .setTimestamp();
 
