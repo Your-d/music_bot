d041f327575a1607abb666a8782386e6fd6f0d14
aldiboytons <76821565+aldiboytons@users.noreply.github.com>
2021-02-03 09:02:27 +0700

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -4,25 +4,25 @@ const sendError = require("../util/error");
 module.exports = {
   info: {
     name: "volume",
-    description: "To change the server song queue volume",
+    description: "Buat ngatur volume",
     usage: "[volume]",
     aliases: ["v", "vol"],
   },
 
   run: async function (client, message, args) {
     const channel = message.member.voice.channel;
-    if (!channel)return sendError("I'm sorry but you need to be in a voice channel to play music!", message.channel);
+    if (!channel)return sendError("Masuk room dulu ASU!", message.channel);
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue) return sendError("There is nothing playing in this server.", message.channel);
+    if (!serverQueue) return sendError("Gaada lagu nyaa.", message.channel);
     if (!args[0])return message.channel.send(`The current volume is: **${serverQueue.volume}**`);
      if(isNaN(args[0])) return message.channel.send(':notes: Numbers only!').catch(err => console.log(err));
-    if(parseInt(args[0]) > 150 ||(args[0]) < 0) return sendError('You can\'t set the volume more than 150. or lower than 0',message.channel).catch(err => console.log(err));
+    if(parseInt(args[0]) > 150 ||(args[0]) < 0) return sendError('Lu bisa atur Volume nya dari 0 sampe 150',message.channel).catch(err => console.log(err));
     serverQueue.volume = args[0]; 
     serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
     let xd = new MessageEmbed()
     .setDescription(`I set the volume to: **${args[0]/1}/100**`)
     .setAuthor("Server Volume Manager", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
-    .setColor("BLUE")
+    .setColor("BLACK")
     return message.channel.send(xd);
   },
 };
