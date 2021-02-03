f0ec2fa3677f0d64a01c5124cb0537c8c69606fa
aldiboytons <76821565+aldiboytons@users.noreply.github.com>
2021-02-03 10:05:53 +0700

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -73,7 +73,7 @@ module.exports = {
     if (serverQueue) {
       serverQueue.songs.push(song);
       let thing = new MessageEmbed()
-      .setAuthor("Song has been added to queue", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
+      .setAuthor("Song has been added to queue", "assets/Music.gif")
       .setThumbnail(song.img)
       .setColor("BLACK")
       .addField("Name", song.title, true)
