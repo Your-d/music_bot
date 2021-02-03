059e877f55eb5139255263a9747ac748e1bff47a
aldiboytons <76821565+aldiboytons@users.noreply.github.com>
2021-02-03 10:12:45 +0700

    Update loop.js

--- commands/loop.js
+++ commands/loop.js
@@ -15,11 +15,11 @@ module.exports = {
             serverQueue.loop = !serverQueue.loop;
             return message.channel.send({
                 embed: {
-                    color: "GREEN",
+                    color: "BLACK",
                     description: `🔁  **|**  Loop is **\`${serverQueue.loop === true ? "enabled" : "disabled"}\`**`
                 }
             });
         };
-    return sendError("There is nothing playing in this server.", message.channel);
+    return sendError("Gaada lagunya bgst.", message.channel);
   },
 };
