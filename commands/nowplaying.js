4c3261a9ae9b9a896b01035bd19f2c5e7dbe4237
aldiboytons <76821565+aldiboytons@users.noreply.github.com>
2021-02-03 09:03:56 +0700

    Update nowplaying.js

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -4,7 +4,7 @@ const sendError = require("../util/error")
 module.exports = {
   info: {
     name: "nowplaying",
-    description: "To show the music which is currently playing in this server",
+    description: "Buat kasi liat yang sekarang lagi di dengerin",
     usage: "",
     aliases: ["np"],
   },
@@ -16,7 +16,7 @@ module.exports = {
     let thing = new MessageEmbed()
       .setAuthor("Now Playing", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
       .setThumbnail(song.img)
-      .setColor("BLUE")
+      .setColor("BLACK")
       .addField("Name", song.title, true)
       .addField("Duration", song.duration, true)
       .addField("Requested by", song.req.tag, true)
