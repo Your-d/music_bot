779aa29131c3312b0f46f65bb474fb20a28b55de
aldiboytons <76821565+aldiboytons@users.noreply.github.com>
2021-03-21 06:51:35 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,13 @@
-module.exports = async (client) => {
-  console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity(" sa.help | SATU ATAP ", {
-    type: "PLAYING",//can be LISTENING, WATCHING, PLAYING, STREAMING
-  });
-};
+const activities_list = [
+    "sa.help", 
+    "SATU ATAP",
+    "https://www.instagram.com/satuatap_dc/", 
+    "TTAX Generation® 2021."
+    ]; // creates an arraylist containing phrases you want your bot to switch through.
+
+bot.on('ready', () => {
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
+        bot.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+    }, 10000); // Runs this every 10 seconds.
+});
