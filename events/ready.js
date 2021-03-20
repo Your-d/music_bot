ded5016a2ab92158afa04264b6e9b008ca7d67b7
aldiboytons <76821565+aldiboytons@users.noreply.github.com>
2021-03-20 15:01:39 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity(" di!help | Aldi Boytons ", {
-    type: "PLAYING",//can be LISTENING, WATCHING, PLAYING, STREAMING
+  await client.user.setActivity(" t1.help | TTAX Generation ", {
+    type: "STREAMING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
