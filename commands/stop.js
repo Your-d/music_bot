ef3eb8eb77c1b6e6e3ffa2c3e6db552c23e0d687
your-daddy-boy <81304629+your-daddy-boy@users.noreply.github.com>
2021-07-11 23:34:17 +0700

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -4,16 +4,16 @@ const sendError = require("../util/error");
 module.exports = {
   info: {
     name: "stop",
-    description: "Buat stop in lagu sama clear queue nya",
+    description: "To stop the music",
     usage: "",
     aliases: [],
   },
 
   run: async function (client, message, args) {
     const channel = message.member.voice.channel
-    if (!channel)return sendError("Masuk room dulu GOBLOK!", message.channel);
+    if (!channel)return sendError("You must be inside VC!", message.channel);
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue)return sendError("Gaada lagunya gmna mau stop kontol,ngadi ngadi ae lu.", message.channel);
+    if (!serverQueue)return sendError("No music in queue", message.channel);
    if(!serverQueue.connection)return
 if(!serverQueue.connection.dispatcher)return
      try{
@@ -25,6 +25,6 @@ if(!serverQueue.connection.dispatcher)return
       }
     message.client.queue.delete(message.guild.id);
     serverQueue.songs = [];
-    message.react("🖕")
+    message.react("😁")
   },
 };
