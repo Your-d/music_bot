b09f9825d239e820507a814f3458f360c2cde938
your-daddy-boy <81304629+your-daddy-boy@users.noreply.github.com>
2021-07-11 23:35:44 +0700

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -4,23 +4,23 @@ const sendError = require("../util/error");
 module.exports = {
   info: {
     name: "volume",
-    description: "Buat ngatur volume",
+    description: "To setting Volume",
     usage: "[volume]",
     aliases: ["v", "vol"],
   },
 
   run: async function (client, message, args) {
     const channel = message.member.voice.channel;
-    if (!channel)return sendError("Masuk room dulu ASU!", message.channel);
+    if (!channel)return sendError("You must be inside VC", message.channel);
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue) return sendError("Gaada lagu nyaa.", message.channel);
+    if (!serverQueue) return sendError("No music in queue", message.channel);
     if (!args[0])return message.channel.send(`The current volume is: **${serverQueue.volume}**`);
      if(isNaN(args[0])) return message.channel.send(':notes: Numbers only!').catch(err => console.log(err));
     if(parseInt(args[0]) > 150 ||(args[0]) < 0) return sendError('Lu bisa atur Volume nya dari 0 sampe 150',message.channel).catch(err => console.log(err));
     serverQueue.volume = args[0]; 
     serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
     let xd = new MessageEmbed()
-    .setDescription(`Udah gua atur jadi : **${args[0]/1}/100**`)
+    .setDescription(`Changed to : **${args[0]/1}/100**`)
     .setAuthor("Server Volume Manager", "https://media0.giphy.com/media/ScBN2m4ZR9NJ3mPdA2/source.gif")
     .setColor("BLACK")
     return message.channel.send(xd);
